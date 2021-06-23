25297dd883bd4bf7deab2690a70a5708ee547ab4
Cris <33341701+crismihailescu@users.noreply.github.com>
2021-06-23 00:47:17 -0700

    Expanded carousel, needs CSS

--- Apps/ui/src/Home/home.js
+++ Apps/ui/src/Home/home.js
@@ -21,6 +21,31 @@ const initialActivities = [
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
   },
   {
+    title: 'Soup Kitchen',
+    type: 'Indoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
+  },
+  {
+    title: 'Volunteer Driver',
+    type: 'Indoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
+  },
+  {
+    title: 'School Dance',
+    type: 'Indoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
+  },
+  {
+    title: 'Retirement Party',
+    type: 'Indoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
+  },
+  {
+    title: 'Childcare',
+    type: 'Indoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
+  },
+  {
     title: 'Park Cleanup2',
     type: 'Outdoor',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Veale_Gardens.JPG/330px-Veale_Gardens.JPG',
@@ -28,6 +53,36 @@ const initialActivities = [
   {
     title: 'Beach Cleanup2',
     type: 'Outdoor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Beach Cleanup2',
+    type: 'Vancouver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Park Cleanup2',
+    type: 'Vancouver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Beach Cleanup2',
+    type: 'Vancouver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Retirement Party',
+    type: 'Vancouver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Beach Cleanup2',
+    type: 'Vancouver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
+  },
+  {
+    title: 'Beach Cleanup2',
+    type: 'Vancouver',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
   },
 ];
@@ -56,7 +111,7 @@ function Home() {
     <div className='home-container' >
       <div className='home-body'>
         <AddActivity name={name} type={type} handleAddActivity={addActivity} onNameChange={handleNameChange} onTypeChange={handleTypeChange} />
-        <ViewActivityByType activities={initialActivities} />
+        <ViewActivityByType activities={activities} />
       </div>
     </div>
   );
