b78c6ba6fdf5f74b6a18cc1dde5a304d1fcd53d5
Annie <53280639+meinna@users.noreply.github.com>
2021-06-25 20:51:27 -0700

    updated images in user dashboard

--- Apps/ui/src/UserDashboard/UserDashboard.js
+++ Apps/ui/src/UserDashboard/UserDashboard.js
@@ -51,53 +51,53 @@ const useStyles = makeStyles((theme) => ({
 
 const userUpcomingActivities = [
     {
-        title: 'ActivityA',
+        title: 'Sundin Park Cleanup',
         type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Veale_Gardens.JPG/330px-Veale_Gardens.JPG',
-    },
-    {
-        title: 'ActivityB',
+        image: 'http://i.huffpost.com/gen/1449440/images/o-PUBLIC-PARKS-facebook.jpg',
+      },
+      {
+        title: 'Waikiki Beach Cleanup',
         type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
-    },
-    {
-        title: 'ActivityC',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gotahoenorth.com%2Fwp-content%2Fuploads%2F2014%2F12%2Fchambers_1.jpg&f=1&nofb=1',
+      },
+      {
+        title: 'Fundraising',
         type: 'Indoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
-    },
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbartowhistorymuseum.org%2Fwp-content%2Fuploads%2F2014%2F07%2Fbake-sale.jpg&f=1&nofb=1',
+      },
 ];
 
 const userPastActivities = [
     {
-        title: 'ActivityD',
-        type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Veale_Gardens.JPG/330px-Veale_Gardens.JPG',
-    },
-    {
-        title: 'ActivityE',
-        type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
-    },
-    {
-        title: 'ActivityF',
+        title: 'Soup Kitchen',
         type: 'Indoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
-    },
-    {
-        title: 'ActivityD',
-        type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Veale_Gardens.JPG/330px-Veale_Gardens.JPG',
-    },
-    {
-        title: 'ActivityE',
-        type: 'Outdoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/YBF_2010_-_Bikini_Bar_perjantaina.jpg/330px-YBF_2010_-_Bikini_Bar_perjantaina.jpg',
-    },
-    {
-        title: 'ActivityF',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fberksfoodbank.org%2Fwp-content%2Fuploads%2F2014%2F01%2FIn-line-at-soup-kitchen.jpeg&f=1&nofb=1',
+      },
+      {
+        title: 'Volunteer Driver',
         type: 'Indoor',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hand_holding_a_red_fundraising_box.jpg/330px-Hand_holding_a_red_fundraising_box.jpg',
-    },
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.driving.co.uk%2Fs3%2Fst-driving-prod%2Fuploads%2F2015%2F02%2FVans.jpg&f=1&nofb=1',
+      },
+      {
+        title: 'School Dance',
+        type: 'Indoor',
+        image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia3.s-nbcnews.com%2Fi%2Fstreams%2F2014%2FMarch%2F140313%2F2D274905359410-today-school-dance-140312.jpg&f=1&nofb=1',
+      },
+      {
+        title: 'Retirement Party',
+        type: 'Indoor',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixfeeds.com%2Fimages%2F22%2F529257%2F1200-535200953-retirement-party-in-office.jpg&f=1&nofb=1',
+      },
+      {
+        title: 'Childcare',
+        type: 'Indoor',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4rfnv3jdfte8qj2229aqgj4h-wpengine.netdna-ssl.com%2Fwp-content%2Fuploads%2F2018%2F06%2F12205709_web1_171212-CMA-M-daycare-1050x700.jpg&f=1&nofb=1',
+      },
+      {
+        title: 'Miller Park Cleanup',
+        type: 'Outdoor',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fspokesman-recorder.com%2Fwp-content%2Fuploads%2F2019%2F05%2FAGA5310.jpg&f=1&nofb=1',
+      },
 ];
 
 function UserDashboard(props) {
