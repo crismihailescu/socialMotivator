be0a15cd469914957033965757edb236f3a1cf63
Cris <33341701+crismihailescu@users.noreply.github.com>
2021-07-17 03:29:25 -0700

    Final working version

--- Apps/ui/src/App/components/Activity.js
+++ Apps/ui/src/App/components/Activity.js
@@ -39,11 +39,7 @@ function Activity(props) {
         </div>
         <div className={classes.sideWrapper}>
             <h4 className={classes.sideBySize}>{props.activityTitle}</h4>
-<<<<<<< HEAD
-            <Enlist className={classes.sideBySize2} picture= {[props.activityImg]} name = {props.activityTitle} type = {props.activityType} location = {props.activityLocation}></Enlist>
-=======
-            <Enlist className={classes.sideBySize2} picture= {[props.activityImg]} name = {props.activityTitle} type = {props.activityType} location = {props.location} start = {props.start} duration = {props.duration}></Enlist>
->>>>>>> b0f13d792da4968858be7c2e08caddac2692b447
+            <Enlist className={classes.sideBySize2} picture= {[props.activityImg]} name = {props.activityTitle} type = {props.activityType} location = {props.activityLocation} start = {props.start} duration = {props.duration}></Enlist>
         </div>
     </div>
     );
