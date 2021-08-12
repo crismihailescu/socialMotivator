eb5527e2f3f2a8b967260c504475de772d772f77
Cris <33341701+crismihailescu@users.noreply.github.com>
2021-08-12 11:13:27 -0400

    Forgot line change

--- Apps/ui/src/App/components/Activity.js
+++ Apps/ui/src/App/components/Activity.js
@@ -43,12 +43,13 @@ function Activity(props) {
     } else {
       returnedDate = returnedDate.substring(0,4) + "/" + returnedDate.substring(4,5) + "/" + returnedDate.substring(5,7) + " at " + returnedTime.substring(0,2) + ":" + returnedTime.substring(2,4)
     }
+
     return (
         <div>
             <div className={classes.activity}>
                 <img alt="Render" style={{ width: '100%', height: '300px', objectFit: 'cover' }} src={props.activityImg} />
                 <h3 className={classes.topLeft}>{props.activityLocation}</h3>
-                <h4 className={classes.bottomLeft}> {props.start} </h4>
+                <h4 className={classes.bottomLeft}> {returnedDate} </h4>
             </div>
             <div className={classes.sideWrapper}>
                 <h4 className={classes.sideBySize}>{props.activityTitle}</h4>
